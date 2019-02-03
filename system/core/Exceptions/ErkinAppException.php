de074f88a35679340a7324d77be8755cda850505
Erkin Eren <ernerkin@gmail.com>
2019-02-03 15:02:28 +0300

    Initial Commit

---
+++ system/core/Exceptions/ErkinAppException.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: erkin
+ * Date: 3.02.2019
+ * Time: 15:00
+ */
+
+namespace ErkinApp\Exceptions;
+
+
+class ErkinAppException extends \Exception
+{
+
+}
--- system/helpers/core_helper.php
+++ system/helpers/core_helper.php
@@ -1,6 +1,96 @@
 <?php
 
 use ErkinApp\ErkinApp;
+use Symfony\Component\HttpFoundation\Response;
+
+/**
+ * @param null $basePath
+ * @throws \ErkinApp\Exceptions\ErkinAppException
+ */
+function handleApp($basePath = null)
+{
+    if ($basePath === null && !defined('BASE_PATH')) {
+        throw new \ErkinApp\Exceptions\ErkinAppException("Base path required !");
+    }
+    $basePath = $basePath === null ? BASE_PATH : $basePath;
+
+    require_once $basePath . '/app/config.php';
+    $loader = require $basePath . '/vendor/autoload.php';
+
+    define('LANGUAGE_PATH', $basePath . '/languages');
+
+
+    $whoops = new \Whoops\Run;
+    $whoops->pushHandler(new \Whoops\Handler\PlainTextHandler());
+    $whoops->register();
+
+
+// Our framework is now handling itself the request
+    $app = ErkinApp();
+
+    $request = $app->Request();
+
+
+    $paths = explode('/', $request->getPathInfo());
+
+    if (count($paths) >= 2) {
+
+        $area = 'Frontend';
+        $defaultController = ROUTE_FRONTEND_DEFAULT_CONTROLLER;
+        $defaultMethod = ROUTE_FRONTEND_DEFAULT_METHOD;
+
+        switch (strtolower($paths[1])) {
+            case strtolower(BACKEND_AREA_NAME):
+                $area = 'Backend';
+                $defaultController = ROUTE_BACKEND_DEFAULT_CONTROLLER;
+                $defaultMethod = ROUTE_BACKEND_DEFAULT_METHOD;
+                break;
+            case 'api':
+                $area = 'Api';
+                $defaultController = ROUTE_API_DEFAULT_CONTROLLER;
+                $defaultMethod = ROUTE_API_DEFAULT_METHOD;
+                break;
+        }
+
+        if (in_array(strtolower($paths[1]), ['frontend', strtolower(BACKEND_AREA_NAME), strtolower(API_AREA_NAME)])) {
+            $controller = !empty($paths[2]) ? ucfirst(strtolower($paths[2])) : $defaultController;
+            $method = isset($paths[3]) && !empty($paths[3]) ? $paths[3] : $defaultMethod;
+        } else {
+            $controller = !empty($paths[1]) ? ucfirst(strtolower($paths[1])) : $defaultController;
+            $method = isset($paths[2]) && !empty($paths[2]) ? $paths[2] : $defaultMethod;
+        }
+
+
+        $classname = 'Application\\Controller\\' . $area . '\\' . $controller;
+
+//    var_dump(method_exists('Application\\Controller\\' . $area . '\\Index', $method));die;
+
+        if (!class_exists($classname)) {
+
+            (new Response())
+                ->setStatusCode(Response::HTTP_BAD_REQUEST)
+                ->setContent("Class <strong> {$classname} </strong> not found !")->send();
+            die;
+        }
+
+        $app->map($request->getPathInfo(),
+            [
+                $classname,
+                $method
+            ]);
+    }
+
+    require_once $basePath . '/app/events.php';
+
+    $app->setCurrentArea($area);
+    $app->setCurrentContoller($classname);
+    $app->setCurrentMethod($method);
+
+    $response = $app->handle($request);
+    $response->send();
+
+
+}
 
 /**
  * @return ErkinApp
